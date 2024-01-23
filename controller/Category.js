aed4dcc940d3dd93bd81ddcc3a2939dcc3f0d2eb
vivek-vimal <vimalvivek50@gmail.com>
2024-01-24 01:24:21 +0530

    some changes

--- controller/Category.js
+++ controller/Category.js
@@ -3,7 +3,7 @@ const { categoryModel } = require("../model");
 module.exports.fetchCategory = async (req, res) => {
   try {
     const response = await categoryModel.find();
-    res.status(200).json({ response, message: "Category fetched successfully" });
+    res.status(200).json(response);
   } catch (err) {
     res.status(400).json(err);
   }
@@ -11,10 +11,10 @@ module.exports.fetchCategory = async (req, res) => {
 
 module.exports.saveCategory = async (req, res) => {
   const { category } = req.body;
-  const categorySave = new category({category})
+  const categorySave = new category({ category });
   try {
     const response = await categorySave.save();
-    res.status(201).json({response,message:"Category saved successfully"})
+    res.status(201).json(response);
   } catch (err) {
     res.status(400).json(err);
   }
