1905c28726e693dbe59cf41a557406302362769d
vivek-vimal <vimalvivek50@gmail.com>
2024-01-24 00:40:10 +0530

    Category added

--- router/CategoryRouter.js
+++ router/CategoryRouter.js
@@ -6,4 +6,4 @@ const categoryRouter = express.Router();
 categoryRouter.get("/", fetchCategory);
 categoryRouter.post("/", saveCategory);
 
-module.exports.categoryRouter
+module.exports = categoryRouter
