16c77b1836112975727202c83e4f20a2ea4980b9
vivek-vimal <vimalvivek50@gmail.com>
2024-01-24 01:33:35 +0530

    some changes

--- model/Category.js
+++ model/Category.js
@@ -3,7 +3,7 @@ const moongose = require("mongoose");
 const { Schema } = moongose;
 
 const categorySchema = new Schema({
-  category: { required: true, type: String },
+  title: { required: true, type: String },
 });
 
 module.exports.categoryModel = moongose.model("category", categorySchema);
